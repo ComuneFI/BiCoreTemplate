e470a9cf921db0ba14892347702ff95d94739b14
Andrea Manzi <manzolo@libero.it>
2018-12-18 07:41:42 +0100

    new release

--- tests/Controller/PannelloAmministrazioneControllerFunctionalTest.php
+++ tests/Controller/PannelloAmministrazioneControllerFunctionalTest.php
@@ -2,9 +2,9 @@
 
 namespace Cdf\BiCoreBundle\Tests\Controller;
 
-use Cdf\BiCoreBundle\Tests\Utils\FifreeTestAuthorizedClient;
+use Cdf\BiCoreBundle\Tests\Utils\BiTestAuthorizedClient;
 
-class PannelloAmministrazioneControllerFunctionalTest extends FifreeTestAuthorizedClient
+class PannelloAmministrazioneControllerFunctionalTest extends BiTestAuthorizedClient
 {
     /*
      * @test
