ddcebeb797b45da579bfed09afb200d4997e470d
Andrea Manzi <andrea.manzi@comune.fi.it>
2019-04-24 16:43:34 +0200

    passaggio a webpack encore

---
+++ public/build/app.00f3b1f0.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([["app"],{ng4s:function(n,o,s){s("sZ/o"),console.log("Hello Webpack Encore! Edit me in assets/js/app.js")},"sZ/o":function(n,o,s){}},[["ng4s","runtime"]]]);
---
+++ public/build/bicore.58120e4e.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([["bicore"],{hA63:function(n,o,i){i("uYsx")},uYsx:function(n,o,i){}},[["hA63","runtime"]]]);
