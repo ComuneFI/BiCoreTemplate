380deaef43a26f1eb54a5760bf7673fa978aec15
Andrea Manzi <manzolo@libero.it>
2019-12-03 22:53:52 +0100

    new core

--- config/bundles.php
+++ config/bundles.php
@@ -13,7 +13,6 @@ return [
     FOS\JsRoutingBundle\FOSJsRoutingBundle::class => ['all' => true],
     Cdf\BiCoreBundle\BiCoreBundle::class => ['all' => true],
     Cdf\PannelloAmministrazioneBundle\PannelloAmministrazioneBundle::class => ['all' => true],
-    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
