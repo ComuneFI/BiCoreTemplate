c9e65ad2f5e46615975bb37db902571c1feb2ccd
Andrea Manzi <andrea.manzi@comune.fi.it>
2020-01-28 14:26:17 +0100

    new core, fix annotation

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -7,16 +7,16 @@ use Symfony\Component\HttpFoundation\Response;
 use \Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-class DefaultController extends AbstractController
-{
+class DefaultController extends AbstractController {
+
     /**
      * Matches / exactly
      *
      * @Route("/", name="welcome")
      */
-    public function index(Request $request)
-    {
+    public function index(Request $request) {
         $template = "Default/index.html.twig";
         return $this->render($template, array());
     }
+
 }
